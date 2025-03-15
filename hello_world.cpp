9d2d2d2cd53ecbbd171b4718a6d3396eada474d5
METEOPuT <119122878+METEOPuT@users.noreply.github.com>
2025-03-15 23:50:29 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,9 @@
-#include<iostream> //подключение библиотек
+#include<iostream> //join library
 #include<string>
 
 int main() {
 	string name; //создание переменной
-	std::cout << "Введите ваше имя: "; //запрос
+	std::cout << "Введите ваше имя: "; //запрос имение пользователя
 	std::cin >> name; //ввод имени
 	std::cout << "Hello world from " << name << std::endl; //вывод приветствия
 	return 0;
