495ba5bd214b3687801f52bf57a22228f3db4238
METEOPuT <iklesh32@gmail.com>
2025-03-15 16:28:01 -0400

    Добавлены комментарии в hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,10 @@
-#include<iostream>
+#include<iostream> //подключение библиотек
 #include<string>
 
 int main() {
-	string name;
-	std::cout << "Введите ваше имя: ";
-	std::cin >> name;
-	std::cout << "Hello world from " << name << std::endl;
+	string name; //создание переменной
+	std::cout << "Введите ваше имя: "; //запрос
+	std::cin >> name; //ввод имени
+	std::cout << "Hello world from " << name << std::endl; //вывод приветствия
 	return 0;
 }
